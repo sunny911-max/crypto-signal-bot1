0dcde682c90b741d64341c6a1adeb8584af9bfad
sunny911-max <realsun911@gmail.com>
2025-07-14 02:44:18 +0530

    Delete signal_logic.py

--- signal_logic.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def check_signals():
-    dummy_signals = [
-        "Buy signal: BTCUSDT at $30,200",
-        "Buy signal: ETHUSDT at $1,900",
-    ]
-    return [random.choice(dummy_signals)] if random.random() > 0.7 else []
