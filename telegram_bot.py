7c6276208f19b4221b3d04dd20c4e9b056881bdf
sunny911-max <realsun911@gmail.com>
2025-07-14 03:25:22 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,9 @@
-import os
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BOT_TOKEN = "7307067620:AAEOHrNskxLEWOcMKvuKtVbrJUYpD0zokMA"
+CHAT_ID = "7545235284"
 
 def send_telegram_message(message):
-    if not BOT_TOKEN or not CHAT_ID:
-        print("❌ BOT_TOKEN or CHAT_ID not set.")
-        return
-
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
